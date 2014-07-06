4b97dc3ea8d043b6d656b7b2e8a027ef6c99483e
ghatamos <ghatamos@yahoo.com>
2014-07-06 13:46:26 +1000

    Create main.c
    add framework for main application code

---
+++ main.c
@@ -0,0 +1,11 @@
+/// \file
+/// \description Entry point for application. Responsible for initialising platform and starting the scheduler.
+
+
+// include files
+
+// file scope constants
+
+// file scope prototypes
+
+// file scope variables
