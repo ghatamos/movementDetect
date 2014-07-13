325ac4216a8499b7367b55e39f4eb3c3da421f46
ghatamos <ghatamos@yahoo.com>
2014-07-13 13:33:16 +1000

    Add the solution file, project file, and main file for the PIR movement detector project

---
+++ PIR_MovementDetector/PIR_MovementDetector.c
@@ -0,0 +1,44 @@
+/*
+ * PIR_MovementDetector.c
+ *
+ * Created: 13/07/2014 1:24:20 PM
+ *  Author: Grant
+ */ 
+
+// include files
+#include <avr/io.h>
+
+// global variables
+
+// constants
+
+// file scope variables
+
+// file scope functions
+/// \desc Setup a working platfrom
+/// \retval	none
+static void PlatformFactory(void);
+
+/////////////////////////////////////////
+
+
+
+
+int main(void)
+{
+	PlatformFactory();
+	
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+// File scope function implementation
+//////////////////////////////////////////////////////////////////////////
+
+void PlatformFactory(void)
+{
+	return;
+}
